0cb62400ba6b4f538e39330aa850bff0033a09e4
Sasha Denouvilliez-Pech <sasha.denouvilliez-pech@mail.mcgill.ca>
2024-01-31 10:25:57 -0500

    get categories, refactoring, utils

--- part_1/categories/test_categories.py
+++ part_1/categories/test_categories.py
@@ -1,1 +1,39 @@
+import requests
+from utils.utils import *
 
+
+# Test get categories
+def test_get_categories_no_params():
+    response = requests.get(API_URL + "/categories")
+    assert response.status_code == 200
+    assert response.json() == default_categories
+
+
+def test_get_categories_by_valid_id():
+    response = requests.get(API_URL + "/categories", params={"id": "1"})
+    assert response.status_code == 200
+    assert response.json() == {"categories": [default_categories["categories"][0]]}
+
+
+def test_get_categories_by_valid_title():
+    response = requests.get(API_URL + "/categories", params={"title": "Home"})
+    assert response.status_code == 200
+    assert response.json() == {"categories": [default_categories["categories"][1]]}
+
+
+def test_get_categories_by_valid_description():
+    response = requests.get(API_URL + "/categories", params={"description": ""})
+    assert response.status_code == 200
+    assert response.json() == default_categories
+
+
+def test_get_categories_by_invalid_param_value():
+    response = requests.get(API_URL + "/categories", params={"id": "0"})
+    assert response.status_code == 200
+    assert response.json() == empty_categories
+
+
+def test_get_categories_by_inexistent_param():
+    response = requests.get(API_URL + "/categories", params={"bad_key": "0"})
+    assert response.status_code == 200
+    assert response.json() == default_categories
---
+++ part_1/utils/utils.py
@@ -0,0 +1,10 @@
+API_URL = "http://localhost:4567"
+
+default_categories = {
+    "categories": [
+        {"id": "1", "title": "Office", "description": ""},
+        {"id": "2", "title": "Home", "description": ""},
+    ]
+}
+
+empty_categories = {"categories": []}
