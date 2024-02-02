fcd0ebeb9f57137a242b82e740994b153c7b22cb
Sasha Denouvilliez-Pech <sasha.denouvilliez-pech@mail.mcgill.ca>
2024-02-02 10:56:17 -0500

    cat, cat_id, and todos

--- part_1/categories/test_categories_id.py
+++ part_1/categories/test_categories_id.py
@@ -1,7 +1,156 @@
 import requests
-from utils.utils import API_URL
+from utils.utils import *
 
 
-def test_api_is_active():
-    response = requests.get(API_URL)
-    assert response.status_code == 200, "API is not active"
+def test_options_categories_id():
+    response = requests.options(API_URL + "/categories/:id")
+    assert response.status_code == 200
+    assert response.headers["Allow"] == "OPTIONS, GET, HEAD, POST, PUT, DELETE"
+
+
+def test_head_categories_id():
+    response = requests.head(API_URL + "/categories/1")
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.text == ""
+
+
+# Test get category by id
+
+
+def test_get_category_id_valid_id():
+    id = 1
+    response = requests.get(API_URL + "/categories/" + str(id))
+    assert response.status_code == 200
+    assert response.json() == {"categories": [default_categories["categories"][0]]}
+
+
+def test_get_category_id_invalid_id():
+    response = requests.get(API_URL + "/categories/" + str(invalid_cat_id))
+    assert response.status_code == 404
+    assert response.json() == cat_id_not_found_id
+
+
+# Test put category with id
+
+
+def test_put_category_id_invalid_id():
+    response = requests.put(API_URL + "/categories/" + str(invalid_cat_id), json={})
+    assert response.status_code == 404
+    assert response.json() == cat_id_invalid_guid
+
+
+def test_put_category_id_no_title():
+    id = 1
+    response = requests.put(API_URL + "/categories/" + str(id), json={})
+    assert response.status_code == 400
+    assert response.json() == no_title_err
+
+
+def test_put_category_id_valid_payload():
+    id = create_category({"title": cat_name}).get("id")
+    response = requests.put(
+        API_URL + "/categories/" + str(id),
+        json={"title": cat_name, "description": cat_desc},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": str(id),
+        "title": cat_name,
+        "description": cat_desc,
+    }
+    delete_category(id)
+
+
+def test_put_category_id_valid_with_id_in_payload():
+    id = create_category({"title": cat_name}).get("id")
+    random_id = 0
+    response = requests.put(
+        API_URL + "/categories/" + str(id),
+        json={"id": str(random_id), "title": cat_name, "description": cat_desc},
+    )
+    print(response.status_code)
+    assert response.status_code == 400
+    assert response.json() == cat_id_put_id_in_payload_err
+    delete_category(id)
+
+
+def test_put_category_id_invalid_field_in_payload():
+    id = create_category({"title": cat_name}).get("id")
+    response = requests.put(
+        API_URL + "/categories/" + str(id),
+        json={"title": cat_name, "description": cat_desc, cat_bad_field: ""},
+    )
+    assert response.status_code == 400
+    assert response.json() == cat_bad_field_err
+    delete_category(id)
+
+
+# Test post category with id
+
+
+def test_post_category_id_invalid_id():
+    response = requests.post(API_URL + "/categories/" + str(invalid_cat_id), json={})
+    assert response.status_code == 404
+    assert response.json() == cat_id_invalid_guid_or_id
+
+
+def test_post_category_id_no_payload():
+    id = 1
+    response = requests.post(API_URL + "/categories/" + str(id), json={})
+    assert response.status_code == 200
+    assert response.json() == default_categories["categories"][0]
+
+
+def test_post_category_id_valid_payload():
+    id = create_category({"title": cat_name}).get("id")
+    response = requests.post(
+        API_URL + "/categories/" + str(id),
+        json={"title": cat_name, "description": cat_desc},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": str(id),
+        "title": cat_name,
+        "description": cat_desc,
+    }
+    delete_category(id)
+
+
+def test_post_category_id_valid_with_id_in_payload():
+    id = create_category({"title": cat_name}).get("id")
+    random_id = 0
+    response = requests.post(
+        API_URL + "/categories/" + str(id),
+        json={"id": str(random_id), "title": cat_name, "description": cat_desc},
+    )
+    assert response.status_code == 400
+    assert response.json() == cat_id_put_id_in_payload_err
+    delete_category(id)
+
+
+def test_post_category_id_invalid_field_in_payload():
+    id = create_category({"title": cat_name}).get("id")
+    response = requests.post(
+        API_URL + "/categories/" + str(id),
+        json={"title": cat_name, "description": cat_desc, cat_bad_field: ""},
+    )
+    assert response.status_code == 400
+    assert response.json() == cat_bad_field_err
+    delete_category(id)
+
+
+# Test delete category by id
+
+
+def test_delete_category_id_invalid_id():
+    response = requests.delete(API_URL + "/categories/" + str(invalid_cat_id))
+    assert response.status_code == 404
+    assert response.json() == cat_id_delete_err
+
+
+def test_delete_category_id_valid_id():
+    id = create_category({"title": cat_name}).get("id")
+    response = requests.delete(API_URL + "/categories/" + str(id))
+    assert response.status_code == 200
+    assert response.text == ""
--- part_1/utils/utils.py
+++ part_1/utils/utils.py
@@ -10,20 +10,70 @@ default_categories = {
     ]
 }
 empty_categories = {"categories": []}
-category_name = "Cat_1"
-category_desc = "This is a nice cat"
+cat_name = "Cat_1"
+cat_desc = "This is a nice cat"
+invalid_cat_id = 0
+cat_bad_field = "idd"
+
+todo_name = "Todo_1"
+todo_desc = "This is a nice todo"
+empty_cat_todos = {"todos": []}
+invalid_todo_id = 0
 
 # Error messages
-post_category_no_title = {"errorMessages": ["title : field is mandatory"]}
+no_title_err = {"errorMessages": ["title : field is mandatory"]}
 post_category_with_id = {
     "errorMessages": [
         "Invalid Creation: Failed Validation: Not allowed to create with id"
     ]
 }
-post_category_bad_field = {"errorMessages": ["Could not find field: idd"]}
+cat_bad_field_err = {"errorMessages": [f"Could not find field: {cat_bad_field}"]}
+cat_id_not_found_id = {
+    "errorMessages": [f"Could not find an instance with categories/{invalid_cat_id}"]
+}
+cat_id_invalid_guid = {
+    "errorMessages": [f"Invalid GUID for {invalid_cat_id} entity category"]
+}
+cat_id_invalid_guid_or_id = {
+    "errorMessages": [
+        f"No such category entity instance with GUID or ID {invalid_cat_id} found"
+    ]
+}
+cat_id_put_id_in_payload_err = {"errorMessages": ["Failed Validation: id should be ID"]}
+cat_id_delete_err = {
+    "errorMessages": [f"Could not find any instances with categories/{invalid_cat_id}"]
+}
+cat_invalid_id_todo_err = {
+    "errorMessages": [
+        f"Could not find parent thing for relationship categories/{invalid_cat_id}/todos"
+    ]
+}
+cat_todo_with_id_err = {"errorMessages": ["Could not find thing matching value for id"]}
+cat_todo_invalid_cat_id_err = {
+    "errorMessages": [
+        f"Could not find any instances with categories/{invalid_cat_id}/todos/{invalid_todo_id}"
+    ]
+}
 
 
 # Helper functions
 def delete_category(id):
     response = requests.delete(API_URL + f"/categories/{id}")
+    assert response.status_code == 200
+
+
+def create_category(payload):
+    response = requests.post(API_URL + "/categories", json=payload)
+    assert response.status_code == 201
+    return response.json()
+
+
+def create_cat_todo(cat_id, todo_payload):
+    response = requests.post(API_URL + f"/categories/{cat_id}/todos", json=todo_payload)
+    assert response.status_code == 201
+    return response.json()
+
+
+def delete_cat_todo(cat_id, todo_id):
+    response = requests.delete(API_URL + f"/categories/{cat_id}/todos/{todo_id}")
     assert response.status_code == 200
