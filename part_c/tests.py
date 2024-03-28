6ee01559304ef0945678a8a69363578209d4bf27
massderrico <massderrico@yahoo.com>
2024-03-28 16:39:30 -0400

    modify and delete test working but not writing to excel file

--- part_c/tests.py
+++ part_c/tests.py
@@ -14,40 +14,41 @@ def test_worksheet_created():
     init_worksheets()
     assert len(workbook.worksheets()) == num_worksheets
 
-# def test_object_transaction(cur_obj, worksheet):
+@pytest.mark.parametrize("cur_obj", range(num_worksheets))
+def test_object_transaction(cur_obj):
 
-#     object_ids = []
-#     current_object_id = None
-
-#     # starts from 0 initial objects in database to n
-#     for i in range(0, n+1, 1):
+    object_ids = []
+    current_object_id = None
+    worksheet = workbook.worksheets()[cur_obj]
+    # starts from 0 initial objects in database to n
+    for i in range(0, n+1, 1):
         
-#         # create an extra dummy object in database
-#         if (i != 0):
-#             response = create_object(cur_obj, payload_object(cur_obj))
-#             object_ids.append(response["id"])
+        # create an extra dummy object in database
+        if (i != 0):
+            response = create_object(cur_obj, payload_object(cur_obj))
+            object_ids.append(response["id"])
 
-#         #start the timer
-#         start_time = time.time_ns()
+        #start the timer
+        start_time = time.time_ns()
 
-#         #create object 
-#         create_response = create_object(cur_obj, payload_object(cur_obj))
-#         current_object_id = create_response["id"]
-#         #modify object 
-#         modify_object(cur_obj, current_object_id, todo_modified_payload)
-#         #delete object 
-#         delete_object(cur_obj, current_object_id)
+        #create object 
+        create_response = create_object(cur_obj, payload_object(cur_obj))
+        current_object_id = create_response["id"]
+        #modify object 
+        modify_object(cur_obj, current_object_id, todo_modified_payload)
+        #delete object 
+        delete_object(cur_obj, current_object_id)
         
-#         # stop timer and get delta time
-#         done_time = time.time_ns()
-#         total_time = done_time - start_time
+        # stop timer and get delta time
+        done_time = time.time_ns()
+        total_time = done_time - start_time
     
-#         #log to worksheet
-#         worksheet.write(i+1, tt_col, total_time/100000.0)
+        #log to worksheet
+        worksheet.write(i+1, tt_col, total_time/100000.0)
 
-#     # Clean Up Objects
-#     for extra_id in object_ids:
-#         delete_object(cur_obj, extra_id)
+    # Clean Up Objects
+    for extra_id in object_ids:
+        delete_object(cur_obj, extra_id)
 
 @pytest.mark.parametrize("cur_obj", range(num_worksheets))
 def test_object_create(cur_obj):
@@ -87,10 +88,11 @@ def test_object_create(cur_obj):
     
     assert True
 
-# def object_delete(cur_obj, worksheet):
+@pytest.mark.parametrize("cur_obj", range(num_worksheets))
+def object_delete(cur_obj):
      # list to save ids
     object_ids = []
-
+    worksheet = workbook.worksheets()[cur_obj]
     # starts from 0 initial objects in database to n
     for i in range(0, n+1, 1):
         response = create_object(cur_obj, payload_object(cur_obj))
@@ -109,34 +111,35 @@ def test_object_create(cur_obj):
         worksheet.write(i+1, n_col, i)
         worksheet.write(i+1, d_time_col, delete_time/100000.0)
 
-# def object_modify(cur_obj, worksheet):
+@pytest.mark.parametrize("cur_obj", range(num_worksheets))
+def object_modify(cur_obj):
 
-#     # list to save ids
-#     object_ids = []
-#     current_object_id = None
-
-#     # starts from 0 initial objects in database to n
-#     for i in range(0, n+1, 1):
+    # list to save ids
+    object_ids = []
+    current_object_id = None
+    worksheet = workbook.worksheets()[cur_obj]
+    # starts from 0 initial objects in database to n
+    for i in range(0, n+1, 1):
         
-#         # create an dummy object in database
-#         response = create_object( cur_obj, payload_object(cur_obj))
-#         current_object_id = response["id"]
-#         object_ids.append(current_object_id)
+        # create an dummy object in database
+        response = create_object( cur_obj, payload_object(cur_obj))
+        current_object_id = response["id"]
+        object_ids.append(current_object_id)
 
-#         #start time
-#         start_time = time.time_ns()
-#         #stop time
-#         modify_object(cur_obj, current_object_id, modified_payload_object(cur_obj))
-#         done_modify = time.time_ns()
+        #start time
+        start_time = time.time_ns()
+        #stop time
+        modify_object(cur_obj, current_object_id, modified_payload_object(cur_obj))
+        done_modify = time.time_ns()
 
-#         #get time delta and write to excel file
-#         modify_time = start_time - done_modify 
-#         worksheet.write(i+1, n_col, i)
-#         worksheet.write(i+1, m_time_col, modify_time/100000.0)
+        #get time delta and write to excel file
+        modify_time = start_time - done_modify 
+        worksheet.write(i+1, n_col, i)
+        worksheet.write(i+1, m_time_col, modify_time/100000.0)
 
-#     # Clean Up Objects
-#     for extra_id in object_ids:
-#         delete_object(cur_obj, extra_id)
+    # Clean Up Objects
+    for extra_id in object_ids:
+        delete_object(cur_obj, extra_id)
 
 
 @pytest.mark.run_last
